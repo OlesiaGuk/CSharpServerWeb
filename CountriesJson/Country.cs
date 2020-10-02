44bb1c0b4df39e3a8df8738335438036245904f8
OlesiaGuk <olesia.v.guk@yandex.ru>
2020-10-02 21:27:42 +0700

    [CountriesJson]

---
+++ CountriesJson/Country.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountriesJson
+{
+    class Country
+    {
+        public string Name { get; set; }
+
+        public long Population { get; set; }
+
+        public List<Currency> Currencies { get; set; }
+
+        public override string ToString()
+        {
+            var currenciesString = new StringBuilder();
+
+            foreach (var c in Currencies)
+            {
+                currenciesString.Append(c).Append(Environment.NewLine);
+            }
+
+            return $"Название: {Name}{Environment.NewLine}" +
+                   $"Численность населения: {Population}{Environment.NewLine}" +
+                   $"Валюта: {currenciesString}";
+        }
+    }
+}
