0dfc3c6d7b39a6bf83c5cd145c22b77ae1fc87a9
OlesiaGuk <olesia.v.guk@yandex.ru>
2020-10-08 10:28:41 +0700

    [CountriesJson] корректировка по замечаниям(1): п.1, 3-6, 8

---
+++ CountriesJsonCore/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountriesJsonCore
+{
+    class Country
+    {
+        public string Name { get; set; }
+
+        public long Population { get; set; }
+
+        public List<Currency> Currencies { get; set; }
+
+        public override string ToString()
+        {
+            var currenciesString = string.Join(Environment.NewLine, Currencies);
+
+            return $"Название: {Name}{Environment.NewLine}" +
+                   $"Численность населения: {Population}{Environment.NewLine}" +
+                   $"Валюта: {currenciesString}";
+        }
+    }
+}
---
+++ CountriesJsonCore/Currency.cs
@@ -0,0 +1,16 @@
+﻿namespace CountriesJsonCore
+{
+    class Currency
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string Symbol { get; set; }
+
+        public override string ToString()
+        {
+            return $"{{code: {Code}, name: {Name}, symbol: {Symbol}}}";
+        }
+    }
+}
